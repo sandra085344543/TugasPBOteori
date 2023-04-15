dfa0582f7e4a2bfe8925867792b51329ab66f000
Amalia Yosandra <amaliasandra858@gmail.com>
2023-04-15 09:39:59 +0700

    Revert "Initial commit"
    This reverts commit 8d1e3172230728da27bd6c1d6bd1bdde010bb233.

---
+++ Nama.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sandra
+ */
+public class Nama {
+    public static void main(String[] args ){
+       System.out.println("Welcome To Java Programing Amalia Yosandra!!!");
+   }
+    
+}
---
+++ TheTree.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sandra
+ */
+public class TheTree {
+    public static void main(String[] args ){
+       System.out.println("I think that i shall never see,");
+        System.out.println("a poem as lovely as a tree.");
+         System.out.println("A tree whose hungry mouth is pressed");
+          System.out.println("Againts the Earth's sweet flowing breast.");
+   }
+    
+}
